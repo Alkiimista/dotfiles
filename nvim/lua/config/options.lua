2af7f18c8c193fb48595c004456632a748b5f6be
Alkiimista <alkiimista@wsl>
2024-10-09 20:32:58 +0200

    wAdd lazy Add additional keymappings Add options

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,23 @@
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true -- expand tab to spaces
+opt.smartindent = true
+
+opt.wrap = false -- disable line wrapping
+opt.swapfile = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.termguicolors = true
+opt.scrolloff = 8
+
+opt.updatetime = 50
