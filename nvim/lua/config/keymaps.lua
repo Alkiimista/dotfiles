8dfd8e84f3b37f2f369bd5d327b5bf44de191360
Duc Nguyen <dducnguyen97@gmail.com>
2024-10-09 21:03:49 +0200

    Add Mason (#2)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>sv", "<C-w>v")
+vim.keymap.set("n", "<leader>sh", "<C-w>h")
+vim.keymap.set("n", "<leader>sq", "<C-w>q")
