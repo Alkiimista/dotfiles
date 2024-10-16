864b3872b708d3f9e68476f05ec45bcf13246347
Duc Nguyen <dducnguyen97@gmail.com>
2024-10-16 23:00:00 +0200

    Add conform + autopair + lualine + nvim-lint (#6)
    * Add treesitter + autopair
    
    * Add conform + nvim-lint
    
    * Add lualine
    
    ---------
    
    Co-authored-by: Alkiimista <alkiimista@wsl>

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+local highlight = { "CursorColumn", "Whitespace" }
+
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {
+		indent = { highlight = highlight },
+		scope = { enabled = true },
+	},
+}
